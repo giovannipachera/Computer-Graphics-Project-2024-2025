56135b4acb14a51626f4c11f1875a208cdb88b38
Giovanni Pachera <giovanni.pachera@mail.polimi.it>
2025-08-19 15:43:18 +0200

    Cross-platform audio playback

--- src/AudioPlayer.cpp
+++ src/AudioPlayer.cpp
@@ -3,16 +3,33 @@
 #include <cstdlib>
 #include <string>
 #include <thread>
+
+#ifdef _WIN32
+#include <windows.h>
+#include <mmsystem.h>
+#pragma comment(lib, "winmm.lib")
+#else
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <signal.h>
 #include <unistd.h>
+#endif
 
 struct AudioPlayer::Impl {
     std::string path;
     bool loop = false;
     std::atomic<bool> playing{false};
     std::thread worker;
+
+#ifdef _WIN32
+    void run() {
+        UINT flags = SND_FILENAME | SND_ASYNC;
+        if (loop) flags |= SND_LOOP;
+        PlaySound(path.c_str(), NULL, flags);
+        while (playing) std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        PlaySound(NULL, NULL, 0); // stop any playing sound
+    }
+#else
     std::string playerCmd;
     pid_t pid = -1;
 
@@ -47,6 +64,7 @@ struct AudioPlayer::Impl {
         do {
             pid = fork();
             if (pid == 0) {
+                setpgid(0, 0); // new process group so we can kill children
                 execl("/bin/sh", "sh", "-c", baseCmd.c_str(), (char*)nullptr);
                 _exit(127);
             } else if (pid > 0) {
@@ -61,6 +79,7 @@ struct AudioPlayer::Impl {
 
         playing = false;
     }
+#endif
 };
 
 AudioPlayer::AudioPlayer() : impl(new Impl) {}
@@ -86,9 +105,14 @@ bool AudioPlayer::play() {
 void AudioPlayer::stop(bool wait) {
     if (!impl->playing) return;
     impl->playing = false;
+#ifdef _WIN32
+    PlaySound(NULL, NULL, 0);
+#else
     if (impl->pid > 0) {
-        kill(impl->pid, SIGKILL);
+        // kill the whole process group to terminate the player
+        kill(-impl->pid, SIGKILL);
     }
+#endif
     if (wait) {
         if (impl->worker.joinable()) impl->worker.join();
     } else {
